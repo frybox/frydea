e428858afa6edf5dfc059f6f34548d35e20ecc38
zenkj <juzejian@gmail.com>
2024-10-04 00:07:31 +0800

    更新对marked的import处理

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import { EditorState } from "@codemirror/state";
 import { EditorView, basicSetup } from "codemirror";
 import { markdown } from "@codemirror/lang-markdown";
+import { marked } from "marked";
+import { vim } from "@replit/codemirror-vim";
 
 const editor = document.getElementById("editor");
 const preview = document.getElementById("preview");
 
 const state = EditorState.create({
     doc: "# Markdown 编辑器\n\n在这里输入你的 Markdown 文本...",
-    extensions: [basicSetup, markdown()]
+    extensions: [vim(), basicSetup, markdown()]
 });
 
 const view = new EditorView({
@@ -16,10 +18,6 @@ const view = new EditorView({
 });
 
 // 实时更新预览
-//view.on('update', () => {
-//    const markdownText = view.state.doc.toString();
-//    preview.innerHTML = marked(markdownText);
-//});
 view.dispatch({
     effects: EditorView.updateListener.of((update) => {
         if (update.changes) {
@@ -29,8 +27,3 @@ view.dispatch({
         }
     })
 });
-
-// 加载 marked.js 进行 Markdown 转换
-const script = document.createElement('script');
-script.src = 'https://cdnjs.cloudflare.com/ajax/libs/marked/2.1.3/marked.min.js';
-document.head.appendChild(script);
