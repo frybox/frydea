75958a7c85ae2da1c5f5f9b7bf814e87e5757fc5
zenkj <juzejian@gmail.com>
2024-10-19 23:36:40 +0800

    添加sqlalchemy的支出

--- frydea/__init__.py
+++ frydea/__init__.py
@@ -2,4 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-import  frydea.views
+from frydea.database import db_session
+
+@app.teardown_appcontext
+def shutdown_dbsession(exception=None):
+    db_session.remove()
+
+# 注册http路由函数
+import frydea.views
